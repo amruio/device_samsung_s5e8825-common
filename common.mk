ead67775380aa651e663d4c7c6c5837be80d0ccd
Ksawlii <ksawery.blaszczak@proton.me>
2025-06-15 12:30:22 +0200

    a53x-common: overlay: Import some overlay values from TrebleDroid
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -107,6 +107,10 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# DOZE
+PRODUCT_PACKAGES += \
+    SamsungDoze
+    
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
