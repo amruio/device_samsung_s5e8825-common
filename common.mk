fc6b7b596d25c2d43eaa348b14ecb95703d075c2
ksawlii <ksawery.blaszczak@proton.me>
2025-06-23 09:04:30 +0200

    a53x-common: Set correct dpi values
    Signed-off-by: ksawlii <ksawery.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
 # Graphics
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 400dpi
+PRODUCT_AAPT_PREF_CONFIG := 450dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Health
